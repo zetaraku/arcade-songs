bb9a2e810eb497ff1a525566f4b69df9105f4c9c
Raku Zeta <zetaraku@gmail.com>
2022-05-02 21:34:18 +0800

    🎉 yarn create nuxt-app arcade-songs

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  customSyntax: 'postcss-html',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-vue'
+  ],
+  // add your custom config here
+  // https://stylelint.io/user-guide/configuration
+  rules: {}
+}
