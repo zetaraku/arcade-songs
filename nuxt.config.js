daf5c1dd751c0a26871c0434ac92a394a7d6c08b
Raku Zeta <zetaraku@gmail.com>
2022-05-04 18:37:08 +0800

    🔧 remove default dark theme settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -86,7 +86,6 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
         dark: {
           primary: colors.blue.darken2,
