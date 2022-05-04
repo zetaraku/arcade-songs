bb0a13f23e9f00fed28939c73e7eba29738d3b3a
Raku Zeta <zetaraku@gmail.com>
2022-05-05 00:35:15 +0800

    ✨ add DarkModeSwitcher component

---
+++ components/DarkModeSwitcher.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import useDarkMode from '~/composables/useDarkMode';
+
+const { isDarkMode } = useDarkMode();
+</script>
+
+<template>
+  <v-btn
+    icon
+    @click="isDarkMode = !isDarkMode"
+  >
+    <v-icon v-if="!isDarkMode">
+      mdi-lightbulb-on-outline
+    </v-icon>
+    <v-icon v-else>
+      mdi-weather-night
+    </v-icon>
+  </v-btn>
+</template>
