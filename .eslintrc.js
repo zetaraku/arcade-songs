ab6a33ce7d439bc199522dcc32e41c1c7f4d00a0
Raku Zeta <zetaraku@gmail.com>
2022-05-05 22:26:41 +0800

    🔧 eslint: fix 'import/extensions' rule for airbnb-base eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,7 @@ module.exports = {
   plugins: [
   ],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'import/extensions': ['error', 'ignorePackages', { js: 'never', ts: 'never' }],
+  },
 };
