0e9d5d2b1360ff0aed9bb2bcd123de208f2f757b
Raku Zeta <zetaraku@gmail.com>
2022-05-05 21:45:26 +0800

    🚚 convert locale files to YAML format

--- locales/index.js
+++ locales/index.js
@@ -1,14 +1,14 @@
 export default [
   {
-    code: 'en', iso: 'en', name: 'English', file: 'en.js',
+    code: 'en', iso: 'en', name: 'English', file: 'en.yaml',
   },
   {
-    code: 'ja', iso: 'ja', name: '日本語', file: 'ja.js',
+    code: 'ja', iso: 'ja', name: '日本語', file: 'ja.yaml',
   },
   {
-    code: 'tc', iso: 'zh-Hant', name: '正體中文', file: 'zh-Hant.js',
+    code: 'tc', iso: 'zh-Hant', name: '正體中文', file: 'zh-Hant.yaml',
   },
   {
-    code: 'sc', iso: 'zh-Hans', name: '简体中文', file: 'zh-Hans.js',
+    code: 'sc', iso: 'zh-Hans', name: '简体中文', file: 'zh-Hans.yaml',
   },
 ];
--- locales/zh-Hans.js
+++
@@ -1,3 +0,0 @@
-export default {
-  hello: '你好世界！',
-};
