7f23262485bc64d1a6762c51f69465bae7e3f8ac
Raku Zeta <zetaraku@gmail.com>
2022-05-10 23:25:33 +0800

    🍱 add site icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,13 @@ export default {
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png?v=1' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png?v=1' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png?v=1' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v=1' },
+      { rel: 'manifest', href: '/site.webmanifest?v=1' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg?v=1' },
+      { rel: 'shortcut icon', href: '/favicon.ico?v=1' },
     ],
   },
 
