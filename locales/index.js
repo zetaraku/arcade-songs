40e3eec171fd73822fd94656fa20b170826ee875
Raku Zeta <zetaraku@gmail.com>
2022-05-11 21:12:53 +0800

    🌐 use ISO locale code as code

--- locales/index.js
+++ locales/index.js
@@ -6,9 +6,9 @@ export default [
     code: 'ja', iso: 'ja', name: '日本語', file: 'ja.yaml',
   },
   {
-    code: 'tc', iso: 'zh-Hant', name: '正體中文', file: 'zh-Hant.yaml',
+    code: 'zh-Hant', iso: 'zh-Hant', name: '正體中文', file: 'zh-Hant.yaml',
   },
   {
-    code: 'sc', iso: 'zh-Hans', name: '简体中文', file: 'zh-Hans.yaml',
+    code: 'zh-Hans', iso: 'zh-Hans', name: '简体中文', file: 'zh-Hans.yaml',
   },
 ];
