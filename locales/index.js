3d89f48732e44f479954c48898b083061ce52124
Raku Zeta <zetaraku@gmail.com>
2022-05-11 21:13:40 +0800

    🌐 add abbr for locales

--- locales/index.js
+++ locales/index.js
@@ -1,14 +1,14 @@
 export default [
   {
-    code: 'en', iso: 'en', name: 'English', file: 'en.yaml',
+    code: 'en', iso: 'en', abbr: 'en', name: 'English', file: 'en.yaml',
   },
   {
-    code: 'ja', iso: 'ja', name: '日本語', file: 'ja.yaml',
+    code: 'ja', iso: 'ja', abbr: 'ja', name: '日本語', file: 'ja.yaml',
   },
   {
-    code: 'zh-Hant', iso: 'zh-Hant', name: '正體中文', file: 'zh-Hant.yaml',
+    code: 'zh-Hant', iso: 'zh-Hant', abbr: 'tc', name: '正體中文', file: 'zh-Hant.yaml',
   },
   {
-    code: 'zh-Hans', iso: 'zh-Hans', name: '简体中文', file: 'zh-Hans.yaml',
+    code: 'zh-Hans', iso: 'zh-Hans', abbr: 'sc', name: '简体中文', file: 'zh-Hans.yaml',
   },
 ];
