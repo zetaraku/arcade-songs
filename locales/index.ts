2d103f575f1681a7a171fd9f6a4babad429bb97d
Raku Zeta <zetaraku@gmail.com>
2022-05-11 21:27:56 +0800

    🔧 add typing for locale settings

--- locales/index.ts
+++ locales/index.ts
@@ -1,3 +1,5 @@
+import type { LocaleObject } from '@nuxtjs/i18n';
+
 export default [
   {
     code: 'en', iso: 'en', abbr: 'en', name: 'English', file: 'en.yaml',
@@ -11,4 +13,4 @@ export default [
   {
     code: 'zh-Hans', iso: 'zh-Hans', abbr: 'sc', name: '简体中文', file: 'zh-Hans.yaml',
   },
-];
+] as LocaleObject[];
