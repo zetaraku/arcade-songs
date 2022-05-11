f1a0e1c4be1d9695cfbbae7d7360d49b40dc11ab
Raku Zeta <zetaraku@gmail.com>
2022-05-11 13:37:55 +0800

    🔧 add locales for vuetify

--- vuetify.options.ts
+++ vuetify.options.ts
@@ -1,7 +1,17 @@
+import { en, ja, zhHant, zhHans } from 'vuetify/src/locale';
+
 export default {
   breakpoint: {},
   icons: {},
-  lang: {},
+  lang: {
+    locales: {
+      en,
+      ja,
+      'zh-Hant': zhHant,
+      'zh-Hans': zhHans,
+    },
+    current: 'en',
+  },
   rtl: false,
   theme: {},
 };
