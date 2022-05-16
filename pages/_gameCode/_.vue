3b2cd3bcdb9279a9a29bb5c5d57b51ccf69bd664
Raku Zeta <zetaraku@gmail.com>
2022-05-16 20:20:54 +0800

    ✨ add catch-all route in /_gameCode/

---
+++ pages/_gameCode/_.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+//! This file is explicitly added to ensure gameCode is properly set for 404 page within this route.
+import { PageNotFoundError } from '~/utils';
+
+throw new PageNotFoundError();
+</script>
