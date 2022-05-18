2724ee2bba034b7d1c32243db3f4977ac258ec0a
Raku Zeta <zetaraku@gmail.com>
2022-05-18 12:28:48 +0800

    ✨ add SheetDataGrid component

---
+++ components/SheetDataGrid.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { inject, Ref } from '@nuxtjs/composition-api';
+import type { Sheet } from '~/types';
+
+const selectedSheets: Ref<Sheet[]> = inject('selectedSheets')!;
+const currentSheets: Ref<Sheet[]> = inject('currentSheets')!;
+
+function toggleSheetSelection(sheet: Sheet) {
+  const index = selectedSheets.value.indexOf(sheet);
+  if (index === -1) {
+    selectedSheets.value.push(sheet);
+  } else {
+    selectedSheets.value.splice(index, 1);
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div
+      class="d-flex flex-wrap justify-center"
+      @contextmenu.prevent
+    >
+      <SheetTile
+        v-for="(sheet, i) in currentSheets"
+        :key="i"
+        :sheet="sheet"
+        :style="{ 'background-color': selectedSheets.includes(sheet) ? 'turquoise' : null }"
+        @click.right.stop="toggleSheetSelection(sheet);"
+      />
+    </div>
+  </div>
+</template>
