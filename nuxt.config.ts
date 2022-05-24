db739623aa75348d05221226973599915703ea90
Raku Zeta <zetaraku@gmail.com>
2022-05-24 23:04:27 +0800

    🔧 disable user scaling on mobile devices

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ const nuxtConfig: NuxtConfig = {
   head: {
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
