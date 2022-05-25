a9384988b975302ef49c5775a15ed8670e125b13
Raku Zeta <zetaraku@gmail.com>
2022-05-25 14:31:34 +0800

    ✨ add LoadingOverlay component

---
+++ components/LoadingOverlay.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-overlay>
+    <div class="text-center">
+      <v-progress-circular indeterminate :size="64" />
+      <v-spacer class="pa-3" />
+      <span>
+        {{ $t('description.loading') }}
+      </span>
+    </div>
+  </v-overlay>
+</template>
