7eafb5bb0ad4e3cb414b1ec84d83afab39ef1364
Raku Zeta <zetaraku@gmail.com>
2022-05-26 20:48:51 +0800

    ♻️ inline menu data

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 /* eslint-disable import/first, import/no-duplicates */
-import { ref, watch, provide, useMeta as useHead, nextTick } from '@nuxtjs/composition-api';
+import { ref, computed, watch, provide, useMeta as useHead, nextTick } from '@nuxtjs/composition-api';
 import useDataStore from '~/stores/data';
 import useVM from '~/composables/useVM';
 import useGameInfo from '~/composables/useGameInfo';
-import useSideNav from '~/composables/useSideNav';
 import LoadingStatus from '~/enums/LoadingStatus';
 import sites from '~/assets/sites';
 import { PageNotFoundError } from '~/utils';
@@ -20,7 +19,30 @@ const {
   siteColor,
   accessCounterUrl,
 } = useGameInfo();
-const { menu } = useSideNav();
+
+const menu = computed(() => [
+  {
+    icon: 'mdi-apps',
+    title: vm.$t('page-title.home'),
+    to: `/${gameCode.value}/`,
+  },
+  {
+    icon: 'mdi-script-text',
+    title: vm.$t('page-title.lists'),
+    to: `/${gameCode.value}/lists/`,
+    disabled: true,
+  },
+  {
+    icon: 'mdi-information-outline',
+    title: vm.$t('page-title.about'),
+    to: `/${gameCode.value}/about/`,
+  },
+  {
+    icon: 'mdi-comment-question',
+    title: vm.$t('page-title.bug-report'),
+    href: 'https://arcade-songs-report.zetaraku.dev/',
+  },
+]);
 
 useHead(() => {
   const i18nHead = vm.$nuxtI18nHead({ addSeoAttributes: true });
@@ -148,7 +170,7 @@ export default defineComponent({
         <v-divider />
       </template>
 
-      <v-list>
+      <v-list v-if="gameCode !== undefined">
         <v-list-item
           v-for="(menuItem, i) in menu"
           :key="i"
