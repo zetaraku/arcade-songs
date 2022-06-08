e4c6137f6992ab2596feb8e35382d88e53f048af
Raku Zeta <zetaraku@gmail.com>
2022-06-08 21:08:43 +0800

    ♻️ inline SheetDataTable headers

--- components/SheetDataTable.vue
+++ components/SheetDataTable.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { watch, inject, Ref } from '@nuxtjs/composition-api';
+import { computed, watch, inject, Ref } from '@nuxtjs/composition-api';
+import useVM from '~/composables/useVM';
 import useGameInfo from '~/composables/useGameInfo';
 import useGameData from '~/composables/useGameData';
-import useSheetTableHeaders from '~/composables/useSheetTableHeaders';
 import useSheetDialog from '~/composables/useSheetDialog';
 import { toPercentageString, validateNoteCounts } from '~/utils';
+import type { DataTableHeader } from 'vuetify';
 import type { Sheet } from '~/types';
 
 const sheets: Ref<Sheet[]> = inject('sheets')!;
@@ -15,19 +16,137 @@ const pageCount: Ref<number> = inject('pageCount')!;
 const currentPage: Ref<number> = inject('currentPage')!;
 const currentSheets: Ref<Sheet[]> = inject('currentSheets')!;
 
+const vm = useVM();
 const { gameCode } = useGameInfo();
 const {
+  getCategoryIndex,
   getVersionAbbr,
+  getVersionIndex,
   getTypeAbbr,
   getTypeIconUrl,
   getTypeIconHeight,
+  getTypeIndex,
   getDifficultyName,
   getDifficultyColor,
   getDifficultyIconUrl,
   getDifficultyIconHeight,
+  getDifficultyIndex,
 } = useGameData();
-const { headers } = useSheetTableHeaders();
 const { viewSheet } = useSheetDialog();
+
+const optionalHeaders: Record<string, DataTableHeader[]> = {
+  maimai: [
+    {
+      text: 'TAP',
+      value: 'noteCounts.tap',
+      width: 50,
+    },
+    {
+      text: 'TAP %',
+      value: 'notePercents.tap',
+      width: 50,
+    },
+    {
+      text: 'HOLD',
+      value: 'noteCounts.hold',
+      width: 50,
+    },
+    {
+      text: 'HOLD %',
+      value: 'notePercents.hold',
+      width: 50,
+    },
+    {
+      text: 'SLIDE',
+      value: 'noteCounts.slide',
+      width: 50,
+    },
+    {
+      text: 'SLIDE %',
+      value: 'notePercents.slide',
+      width: 50,
+    },
+    {
+      text: 'TOUCH',
+      value: 'noteCounts.touch',
+      width: 50,
+    },
+    {
+      text: 'TOUCH %',
+      value: 'notePercents.touch',
+      width: 50,
+    },
+    {
+      text: 'BREAK',
+      value: 'noteCounts.break',
+      width: 50,
+    },
+    {
+      text: 'BREAK %',
+      value: 'notePercents.break',
+      width: 50,
+    },
+    {
+      text: vm.$t('term.totalNotes') as string,
+      value: 'noteCounts.total',
+      width: 50,
+    },
+  ],
+};
+const headers = computed<DataTableHeader[]>(() => [
+  {
+    text: 'No.',
+    value: 'songNo',
+    width: 10,
+  },
+  {
+    text: vm.$t('term.category') as string,
+    value: 'category',
+    width: 200,
+    sort: (a: string, b: string) => getCategoryIndex(a) - getCategoryIndex(b),
+  },
+  {
+    text: vm.$t('term.title') as string,
+    value: 'title',
+    width: 300,
+  },
+  {
+    text: vm.$t('term.type') as string,
+    value: 'type',
+    width: 75,
+    sort: (a: string, b: string) => getTypeIndex(a) - getTypeIndex(b),
+  },
+  {
+    text: vm.$t('term.difficulty') as string,
+    value: 'difficulty',
+    width: 100,
+    sort: (a: string, b: string) => getDifficultyIndex(a) - getDifficultyIndex(b),
+  },
+  {
+    text: vm.$t('term.level') as string,
+    value: 'levelValue',
+    width: 100,
+  },
+
+  ...(optionalHeaders[gameCode.value!] ?? []),
+
+  {
+    text: vm.$t('term.bpm') as string,
+    value: 'bpm',
+    width: 50,
+  },
+  {
+    text: vm.$t('term.noteDesigner') as string,
+    value: 'noteDesigner',
+    width: 150,
+  },
+  {
+    text: vm.$t('term.version') as string,
+    value: 'version',
+    width: 150,
+    sort: (a: string, b: string) => getVersionIndex(a) - getVersionIndex(b),
+  },
+]);
 
 function getItemClass(sheet: Sheet) {
   return selectedSheets.value.includes(sheet) ? 'selected-sheet' : '';
