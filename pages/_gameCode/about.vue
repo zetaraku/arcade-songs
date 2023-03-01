2470126d6cf06aa996a9dc9811032e6bc570373c
Raku Zeta <zetaraku@gmail.com>
2023-03-02 00:46:25 +0800

    ✨ update about page

--- pages/_gameCode/about.vue
+++ pages/_gameCode/about.vue
@@ -223,6 +223,15 @@ export default defineComponent({
     <h3>{{ $t('page.about.updateRecord') }}</h3>
     <v-timeline dense align-top class="mt-2">
       <v-timeline-item small color="grey">
+        <span class="font-weight-bold">2023/03/02 － v3.5.2 Update</span>
+        <ul>
+          <li>
+            i18n: (Site) Indonesian (id) locale is now available!
+            <i>(Thanks <a href="https://github.com/echocentrical" target="_blank">@echocentrical</a> !)</i>
+          </li>
+        </ul>
+      </v-timeline-item>
+      <v-timeline-item small color="grey">
         <span class="font-weight-bold">2023/03/01 － v3.5.1 Update</span>
         <ul>
           <li>
