3bf3c31bf5e37b6535299be068e28e74e189830d
Raku Zeta <zetaraku@gmail.com>
2024-08-20 15:18:42 +0800

    🩹 freeze data instead to prevent accidentally mutation with super filter

--- utils/data.ts
+++ utils/data.ts
@@ -1,4 +1,3 @@
-import { markRaw } from '@nuxtjs/composition-api';
 import { computeSheetExpr, validateNoteCounts } from '~/utils/sheet';
 import type { Data } from '~/types';
 
@@ -56,10 +55,16 @@ export function preprocessData(data: Data, dataSourceUrl: string, gameCode: stri
         console.warn('Invalid note counts:', sheet.sheetExpr, sheet.noteCounts);
       }
 
-      markRaw(sheet);
+      Object.freeze(sheet.noteCounts);
+      Object.freeze(sheet.notePercents);
+      Object.freeze(sheet.regions);
+
+      Object.freeze(sheet);
     }
 
-    markRaw(song);
+    Object.freeze(song.sheets);
+
+    Object.freeze(song);
   }
 
   data.songs.reverse();
@@ -68,20 +73,30 @@ export function preprocessData(data: Data, dataSourceUrl: string, gameCode: stri
   data.sheets = data.songs.flatMap((song) => song.sheets);
 
   for (const category of data.categories) {
-    markRaw(category);
+    Object.freeze(category);
   }
   for (const version of data.versions) {
-    markRaw(version);
+    Object.freeze(version);
   }
   for (const type of data.types) {
     type.iconUrl = resolveUrl(type.iconUrl, `${dataSourceUrl}/img/`);
-    markRaw(type);
+    Object.freeze(type);
   }
   for (const difficulty of data.difficulties) {
     difficulty.iconUrl = resolveUrl(difficulty.iconUrl!, `${dataSourceUrl}/img/`);
-    markRaw(difficulty);
+    Object.freeze(difficulty);
   }
   for (const region of data.regions) {
-    markRaw(region);
+    Object.freeze(region);
   }
+
+  Object.freeze(data.songs);
+  Object.freeze(data.sheets);
+  Object.freeze(data.categories);
+  Object.freeze(data.versions);
+  Object.freeze(data.types);
+  Object.freeze(data.difficulties);
+  Object.freeze(data.regions);
+
+  Object.freeze(data);
 }
