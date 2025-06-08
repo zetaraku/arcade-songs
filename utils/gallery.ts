cffedacef82b6ef87acff685b3984759baa5d5a7
Raku Zeta <zetaraku@gmail.com>
2025-06-09 00:37:06 +0800

    ⚡️ improve gallery loading performance

--- utils/gallery.ts
+++ utils/gallery.ts
@@ -3,19 +3,17 @@ import type { RawGallery, Gallery, Sheet } from '~/types';
 
 // eslint-disable-next-line import/prefer-default-export
 export function buildGallery(data: RawGallery, sheets: Sheet[]): Gallery {
-  function getSheet(sheetExpr: string): Sheet {
-    return sheets.find(
-      (sheet) => sheet.sheetExpr === sheetExpr,
-    ) ?? makeDummySheet(sheetExpr);
-  }
-
   const lists = data;
+
+  const sheetMap = new Map(sheets.map((sheet) => [sheet.sheetExpr, sheet]));
 
   return lists.map((list) => ({
     ...list,
     sections: list.sections.map((section) => ({
       ...section,
-      sheets: section.sheets?.map((sheetExpr) => getSheet(sheetExpr)),
+      sheets: section.sheets?.map(
+        (sheetExpr) => sheetMap.get(sheetExpr) ?? makeDummySheet(sheetExpr),
+      ),
     })),
   }));
 }
